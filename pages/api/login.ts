29e4e5845b325226de75b83f6a72c14c35c3af40
Nguyen Hoai Khang <nguyenhoaikhang37@gmail.com>
2021-11-30 16:36:03 +0700

    update proxy

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -2,6 +2,10 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import httpProxy from 'http-proxy'
 
+type Data = {
+	message: string
+}
+
 export const config = {
 	api: {
 		bodyParser: false,
@@ -10,21 +14,23 @@ export const config = {
 
 const proxy = httpProxy.createProxyServer()
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+	if (req.method !== 'POST') {
+		return res.status(404).json({ message: 'method not supported' })
+	}
+
 	return new Promise((resolve) => {
 		// dont send cookies to API server
 		req.headers.cookie = ''
 
-		// /api/students
+		proxy.once('proxyRes', () => {
+			resolve(true)
+		})
 
 		proxy.web(req, res, {
 			target: process.env.API_URL,
 			changeOrigin: true,
-			selfHandleResponse: false,
-		})
-
-		proxy.once('proxyRes', () => {
-			resolve(true)
+			selfHandleResponse: true,
 		})
 	})
 }
